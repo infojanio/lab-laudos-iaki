a5aeb307dd27d2272254e8981b14ded0bc70bdf1
info.janio@gmail.com <info.janio@gmail.com>
2026-03-05 15:50:18 -0300

    env

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,11 +4,16 @@ const envSchema = z.object({
   VITE_API_URL: z.string().url(),
 })
 
-const _env = envSchema.safeParse(import.meta.env)
+const env = {
+  VITE_API_URL: import.meta.env.VITE_API_URL,
+}
 
-if (!_env.success) {
-  console.error("Invalid environment variables", _env.error.format())
+const parsed = envSchema.safeParse(env)
+
+if (!parsed.success) {
+  console.error("Invalid environment variables", parsed.error.format())
+  console.log("ENV RECEBIDA:", env)
   throw new Error("Invalid environment variables.")
 }
 
-export const env = _env.data
+export default parsed.data
