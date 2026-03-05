0f062d14672e7f90726255b3ce0480b0bf254a3f
info.janio@gmail.com <info.janio@gmail.com>
2026-03-05 16:28:01 -0300

    vite_api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from "axios"
+import { env } from "@/env"
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: env.VITE_API_URL,
   withCredentials: false,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 })
